ad6101aa513dc25359ee786bda4012dffd383bcb
utatsineza <h.utatsinez@alustudent.com>
2024-10-25 07:18:37 -0700

    api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,23 +2,27 @@
 """Script that fetch 10 hot post for a given subreddit."""
 import requests
 
-
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
+    # Define the URL and headers to avoid following redirects
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    # Make a GET request to Reddit's API
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    # Check if the subreddit is valid (status code should be 200)
     if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+        data = response.json()
+        posts = data['data']['children']
+        
+        # If there are posts, print their titles
+        if len(posts) > 0:
+            for post in posts[:10]:
+                print(post['data']['title'])
+            print("OK")  # Explicitly printing "OK" for valid subreddits
+        else:
+            print("None")  # Handle case where subreddit is valid but has no posts
     else:
-        print(None)
+        # Invalid subreddit case
+        print("None")
+
