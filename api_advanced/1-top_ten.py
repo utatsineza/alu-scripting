a06c33df579a384def06c315bbfe84954d22cbb8
utatsineza <h.utatsinez@alustudent.com>
2024-10-26 13:50:59 -0700

    api_advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,9 +8,12 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    ok= "OK"
     if response.status_code != 200:
-        print(OK)
+        print(None)
         return
     posts = response.json()['data']['children']
     for post in posts:
         print(post['data']['title'])
+    else:
+        print(Ok)
